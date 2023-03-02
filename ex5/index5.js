1262f3dff74a5486094db84b65fdc15105e09172
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-03-02 10:09:17 +0300

    Вывод элементов массива

---
+++ ex5/index5.js
@@ -0,0 +1,11 @@
+
+function positiveSum(arr) {
+   let sum = 0 
+   for(i = 0; i < arr.length; i++){
+    if(arr[i] >= 0){
+        sum+=arr[i]
+    }
+  }
+  return sum
+}
+  alert(positiveSum([1,-4,7,12]))
