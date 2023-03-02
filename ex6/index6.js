0a406440928f2bc50e445477ae3ebfe13abbdeb4
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-03-02 16:15:43 +0300

    Перебор овец

---
+++ ex6/index6.js
@@ -0,0 +1,16 @@
+function countSheeps(arrayOfSheep) {
+    let SumSheep = 0;
+    for (i = 0; i< arrayOfSheep.length; i++){
+        if( arrayOfSheep[i] === true){
+            SumSheep+=1;
+        }
+    }
+    return SumSheep
+  }
+console.log(countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ])
+);
