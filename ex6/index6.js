e17656b80cc23e2e8779966f40e1d63291101df9
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-03-02 16:30:57 +0300

    Поиск овец в массиве

--- ex6/index6.js
+++ ex6/index6.js
@@ -1,11 +1,14 @@
-function countSheeps(arrayOfSheep) {
-    let SumSheep = 0;
-    for (i = 0; i< arrayOfSheep.length; i++){
-        if( arrayOfSheep[i] === true){
-            SumSheep+=1;
-        }
-    }
-    return SumSheep
+// function countSheeps(arrayOfSheep) {
+//     let SumSheep = 0;
+//     for (i = 0; i< arrayOfSheep.length; i++){
+//         if( arrayOfSheep[i] === true){
+//             SumSheep+=1;
+//         }
+//     }
+//     return SumSheep
+//   }
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
   }
 console.log(countSheeps([true,  true,  true,  false,
     true,  true,  true,  true ,
