d758577c8ae6254914f91acdda5b17d1735378d9
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-03-07 11:11:02 +0300

    Объекты, методы объекта и this

--- object/Object_metod_This.js
+++ object/Object_metod_This.js
@@ -29,22 +29,48 @@
 // alert(ca)
 // alert(calculator.mul())
 
-let ladder = {
-    step: 0,
-    up() {
-      this.step++;
-      return this
-    },
-    down() {
-      this.step--;
-      return this
-    },
-    showStep: function() { // показывает текущую ступеньку
+// let ladder = {
+//     step: 0,
+//     up() {
+//       this.step++;
+//       return this
+//     },
+//     down() {
+//       this.step--;
+//       return this
+//     },
+//     showStep: function() { // показывает текущую ступеньку
     
-    alert(this.step );
-    return this;
-    }
-  };
-  ladder.up().up().down().showStep().down().showStep();
+//     alert(this.step );
+//     return this;
+//     }
+//   };
+//   ladder.up().up().down().showStep().down().showStep();
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+// let user = {
+//   name: 'Vladimir',
+//   age: 30,
+//   sayHi(){
+//     alert(this.name)// если сделать user.name то будет ошибка
+//   }
+// }
+// let admin = user
+// user = null
+// admin.sayHi()
+// console.log(user);
+
+// let user = {
+//   name:'Vladimir',
+//   age: 22, 
+//   salary: 26400,
+//   countSalary(){
+//     return this.salary+= 2000    
+//   }
+// }
+// let newWorker = user
+// newWorker.name = "joker"
+// user = null
+// console.log(newWorker.countSalary());
+// console.log(newWorker)
+
+
