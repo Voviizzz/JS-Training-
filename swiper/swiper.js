a88f079bbf54f2284094b658483ad647ef9415d0
Vladimir Zaytsev <ivovochka48@gmial.com>
2023-03-24 14:43:05 +0300

    Swiper слайдер библиотека

---
+++ swiper/swiper.js
@@ -0,0 +1,11 @@
+
+
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
